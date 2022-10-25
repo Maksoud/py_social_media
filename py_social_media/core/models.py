f7c4d6f2b55cf987025d1c4adedacc3e12626605
Renée Rodrigues <renee.rodrigues@edge.ufal.br>
2022-10-25 08:44:54 -0300

    Account settings

--- py_social_media/core/models.py
+++ py_social_media/core/models.py
@@ -5,11 +5,12 @@ User = get_user_model()
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_user = models.IntegerField()
-    bio = models.TextField(blank=True)
+
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user    = models.IntegerField()
+    bio        = models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
-    location = models.CharField(max_length=100, blank=True)
+    location   = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.user.username
